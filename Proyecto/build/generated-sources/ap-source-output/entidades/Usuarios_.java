beac422015ed6acd2e3564cc275e0e9c65b35d7d
Tom van Greevenbroek <tom.v.g@outlook.com>
2021-04-26 13:29:09 +0200

    :art: Registro y editar usuario

--- Proyecto/build/generated-sources/ap-source-output/entidades/Usuarios_.java
+++ Proyecto/build/generated-sources/ap-source-output/entidades/Usuarios_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-26T08:59:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-26T09:32:32")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
