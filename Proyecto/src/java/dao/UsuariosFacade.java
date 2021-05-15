bd3610ccc246f20a3793d9cff366ad4bcad2fd3b
UMAKeyla <48551701+UMAKeyla@users.noreply.github.com>
2021-05-16 01:50:39 +0200

    Funcionalidad de mensajes añadida pero...
    Cuando se envia un mensaje, este se guarda en la base de datos, pero al redirigir a la página de mensajes, sale un NullPointerException

--- Proyecto/src/java/dao/UsuariosFacade.java
+++ Proyecto/src/java/dao/UsuariosFacade.java
@@ -48,4 +48,20 @@ public class UsuariosFacade extends AbstractFacade<Usuarios> {
             return listaUsuarios.get(0);
         }
     }
+    
+    public Usuarios getTeleoperador(){
+        Query q;
+        List<Usuarios> listaUsuarios;
+
+        q = this.em.createQuery("SELECT a FROM Usuarios a WHERE a.rol = 3");
+        //q = this.getEntityManager().createNamedQuery("Usuarios.findByEmailAndPassword");
+
+        listaUsuarios = q.getResultList();
+
+        if (listaUsuarios == null || listaUsuarios.isEmpty()) {
+            return null;
+        } else {
+            return listaUsuarios.get(0);
+        }
+    }
 }
